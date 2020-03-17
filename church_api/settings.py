9ab5eb472f76b6e55a89ab4ef9f8e0153ed98e9e
andrey <esin.andrey@ylab.io>
2020-03-17 14:59:40 +0700

    SSL_REDIRECT remove

--- church_api/settings.py
+++ church_api/settings.py
@@ -13,8 +13,6 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-SECURE_SSL_REDIRECT = env.bool('SSL_REDIRECT', True)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
 

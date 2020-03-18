a9947b1cffdf3bdda254f48014e6e663220e40b6
andrey <esin.andrey@ylab.io>
2020-03-18 20:11:32 +0700

    profile image_url field

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = models.Profile
         fields = '__all__'
+
+    image_url = serializers.SerializerMethodField()
+
+    def get_image_url(self, obj):
+        return self.context['request'].build_absolute_uri(obj.image.url)

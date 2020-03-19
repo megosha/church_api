dd49fbc7c10cc95552b565e35ea820e23f3abbc4
andrey <esin.andrey@ylab.io>
2020-03-19 16:40:06 +0700

    HTTP_X_FORWARDED_PROTO https

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,3 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Profile
         fields = '__all__'
-
-    image_url = serializers.SerializerMethodField()
-
-    def get_image_url(self, obj):
-        return self.context['request'].build_absolute_uri(obj.image.url)
--- church_api/settings.py
+++ church_api/settings.py
@@ -141,3 +141,5 @@ LOGGING = {
         },
     },
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

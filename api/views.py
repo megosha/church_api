12db39e05f31e3f0bc0727501c60cddc6a7873d0
andrey <esin.andrey@ylab.io>
2020-03-30 18:04:35 +0700

    form fix

--- api/views.py
+++ api/views.py
@@ -19,23 +19,27 @@ class ProfileViewSet(viewsets.ModelViewSet):
 class FormViewSet(APIView):
     permission_classes = [AllowAny]
 
+    def resp(self, status: bool = True, msg: str = 'OK'):
+        key = 'response' if status else 'error'
+        return HttpResponse(json.dumps({key: msg}))
+
     def post(self, request):
         # formoid.js: var API_URL = ('https:' == location.protocol ? 'https:' : 'http:') + '//api.electis.ru/form/';
         try:
             form = json.loads(next(iter(request.data)))['form']
             title = form['title']
             data = form['data']
+            html = f'<h2>{title}</h2>' + '<br>'.join([f"{d[0]}: {d[1]}" for d in data])
         except Exception as e:
-            return HttpResponse('{"error": "Ошибка данных: {e}"}'.format(e=str(e)))
+            return self.resp(False, f'Ошибка данных: {e}')
         emails = ('andrey@ngbarnaul.ru',)
         form_obj = models.Form.objects.create(title=title, text=json.dumps(data, ensure_ascii=False))
-        html = f'<h2>{title}</h2>' + '<br>'.join([f"{d[0]}: {d[1]}" for d in data])
         mail = EmailMultiAlternatives(title, html, settings.EMAIL_HOST_USER, emails)
         mail.content_subtype = "html"
         try:
             mail.send(fail_silently=False)
         except Exception as e:
-            return HttpResponse('{"error": "Ошибка отправки сообщения: {e}"}'.format(e=str(e)))
+            return self.resp(False, f'Ошибка отправки сообщения: {e}')
         form_obj.sended = True
         form_obj.save()
-        return HttpResponse('{"response": "OK"}')
+        return self.resp()

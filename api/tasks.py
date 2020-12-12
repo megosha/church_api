0005ff0c4f9ea971f152415f6e09d3e959ba7020
Andrey <andrey@electis.ru>
2020-12-12 07:50:35 +0700

    celery sheduler, task settings

--- api/tasks.py
+++ api/tasks.py
@@ -7,7 +7,9 @@ from front import methods
 @app.task(name="api.tasks.time_to_pray", ignore_result=True)
 def time_to_pray():
     token = methods.TGram.get_token()
+    chat_id = methods.get_set('time_to_pray_chat_id')
+    text = methods.get_set('time_to_pray_text')
     result = requests.get(f'https://api.telegram.org/bot{token}/sendMessage', params=dict(
-        chat_id='@memberbook',
-        text='Hello world!'
+        chat_id=chat_id,
+        text=text
     ))

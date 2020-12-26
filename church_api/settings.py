82678f61c320e283656cdc3e48b13358f2dd5b93
Andrey <andrey@electis.ru>
2020-12-26 09:21:30 +0700

    multi site all back

--- church_api/settings.py
+++ church_api/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'front.middleware.SiteSelectMiddleware',
 ]
 
 ROOT_URLCONF = 'church_api.urls'
---
+++ front/middleware.py
@@ -0,0 +1,20 @@
+from api import models
+
+
+class SiteSelectMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Code to be executed for each request before  the view (and later middleware) are called.
+        try:
+            domain = request.META['HTTP_HOST']
+            site = models.Site.objects.get(domain=domain)
+        except:
+            site = models.Site.objects.filter().first()
+        request.site = site
+
+        response = self.get_response(request)
+        # Code to be executed for each request/response after the view is called.
+
+        return response

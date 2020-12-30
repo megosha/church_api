b7172d4f4dd7174149306754a2160faa8343d949
andrey <esin.andrey@ylab.io>
2020-12-30 07:47:24 +0700

    take_meter_value

--- api/tasks.py
+++ api/tasks.py
@@ -1,5 +1,6 @@
-import requests
 import logging
+
+from emoji import emojize
 
 from church_api.celery import app
 from front import methods
@@ -7,17 +8,23 @@ from front import methods
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
+@app.task(name="api.tasks.take_meter_value", ignore_result=True)
+def take_meter_value():
+    logger = logging.getLogger(__name__)
+    logger.info("take_meter_value start")
+    chat_id = methods.get_set('TTP_ID')
+    text = emojize('Не забудьте передать показания счётчиков. :cat_face:')
+    result = methods.TGram.send_message(text, chat_id)
+    logger.info(f"take_meter_value end: {result}")
+
+
 @app.task(name="api.tasks.time_to_pray", ignore_result=True)
 def time_to_pray():
     logger = logging.getLogger(__name__)
     logger.info("time_to_pray start")
-    token = methods.TGram.get_token()
     chat_id = methods.get_set('TTP_ID')
     text = methods.get_set('TTP_TEXT')
-    result = requests.get(f'https://api.telegram.org/bot{token}/sendMessage', params=dict(
-        chat_id=chat_id,
-        text=text
-    ))
+    result = methods.TGram.send_message(text, chat_id)
     logger.info(f"time_to_pray end: {result}")
 
 

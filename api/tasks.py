abff1068b648ec3ef12937e2fcb540477b1148d9
andrey <esin.andrey@ylab.io>
2021-02-04 07:59:39 +0700

    tasks say2group

--- api/tasks.py
+++ api/tasks.py
@@ -6,31 +6,25 @@ from church_api.celery import app
 from front import methods
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @app.task(name="api.tasks.take_meter_value", ignore_result=True)
 def take_meter_value():
-    logger = logging.getLogger(__name__)
     logger.info("take_meter_value start")
-    chat_id = methods.get_set('TTP_ID')
     text = emojize('Не забудьте передать показания счётчиков. :cat_face:')
-    result = methods.TGram.send_message(text, chat_id)
-    logger.info(f"take_meter_value end: {result}")
+    say2group(text)
 
 
 @app.task(name="api.tasks.time_to_pray", ignore_result=True)
 def time_to_pray():
-    logger = logging.getLogger(__name__)
     logger.info("time_to_pray start")
-    chat_id = methods.get_set('TTP_ID')
     text = methods.get_set('TTP_TEXT')
-    result = methods.TGram.send_message(text, chat_id)
-    logger.info(f"time_to_pray end: {result}")
+    say2group(text)
 
 
 @app.task(name="api.tasks.say2boss", ignore_result=True)
 def say2boss(text):
-    logger = logging.getLogger(__name__)
     logger.info("say2boss start")
     result = methods.TGram.say2boss(text)
     logger.info(f"say2boss end: {result}")
@@ -38,7 +32,14 @@ def say2boss(text):
 
 @app.task(name="api.tasks.send_email", ignore_result=True)
 def send_email(title, text, emails=None, as_html=False):
-    logger = logging.getLogger(__name__)
     logger.info(f"send_email start ('{title}', {emails})")
     result = methods.send_email(title, text, emails, as_html)
     logger.info(f"send_email end: {result}")
+
+
+@app.task(name="api.tasks.say2group", ignore_result=True)
+def say2group(text, chat_id=None):
+    logger.info("say2group start")
+    chat_id = chat_id or methods.get_set('TTP_ID')
+    result = methods.TGram.send_message(text, chat_id)
+    logger.info(f"say2group end: {result}")

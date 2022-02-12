d1fffe8efd219601e0c4694a71b613806fe686ae
Andrey <Andrey@electis.ru>
2022-02-12 09:37:11 +0700

    parse_tg in progress

--- api/management/commands/parse_tg.py
+++ api/management/commands/parse_tg.py
@@ -8,25 +8,32 @@ from api import models
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        config = models.Config.command('parse_bible')
-        if not config:
-            print('parse_tg settings not found')
+        config = models.Config.command('parse_tg')
+        if not config or not ('api_id' in config and 'api_hash' in config and 'channel' in config):
+            print('parse_tg settings not found, example: '
+                  '{"parse_tg": {"api_id": "api_id", "api_hash": "api_hash", "channel": "channel"}}')
             return
-        if not ('api_id' in config and 'api_hash' in config and 'channel' in config):
-            print('parse_tg settings must include: api_id, api_hash, channel')
-            return
-        client = TelegramClient('parse_tg', config['api_id'], config['api_hash'])
-        asyncio.run(self.parse_channel(client, config))
+        asyncio.run(self.parse_channel(config))
 
-    async def parse_channel(self, client: TelegramClient, config: dict):
+    async def parse_channel(self, config: dict):
+        parse_book = 'Исход'
+        client = TelegramClient('parse_tg', config['api_id'], config['api_hash'])
         async with client:
+            right_book = False
             async for message in client.iter_messages(config['channel'], reverse=True):
                 if message.audio:
                     book = message.audio.attributes[0].performer
-                    part = message.audio.attributes[0].title
-                    file_name = message.audio.attributes[1].file_name
-                    text = message.text
-                    # path = await message.download_media()
-                    print(book, part, file_name, text[:10])
+                    if book == parse_book:
+                        if not right_book:
+                            right_book = True
+                        part = message.audio.attributes[0].title
+                        file_name = message.audio.attributes[1].file_name
+                        text = message.text
+                        path = await message.download_media()
+                        # TODO переместить файл в медиа, создать модель, прописать js
+                        print(book, part, file_name, text[:10])
+                    elif right_book:
+                        break
+                    print(book, message.text[:10])
                 else:
-                    print(message.text)
+                    print(message.text[:20])

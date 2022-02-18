5f091d15e1f9725c6aca5bd47222bbe122d89674
Andrey <Andrey@electis.ru>
2022-02-18 18:47:47 +0700

    parse_tg

--- api/management/commands/parse_tg.py
+++ api/management/commands/parse_tg.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 
 from django.core.management.base import BaseCommand
 from telethon import TelegramClient
@@ -17,23 +18,60 @@ class Command(BaseCommand):
 
     async def parse_channel(self, config: dict):
         parse_book = 'Исход'
+        section = models.NewsSection.objects.get(title='Медиа')
+        title = f'{parse_book} - Библия - аудиоверсия - РБО'
+        text_start = f'Библия - {parse_book} - аудиоверсия - Современный русский перевод Русского Библейского Общества'
+        author = 'Телеграм "Слушать Библию" @bible_fj'
+        media_path = 'media/books'
+        audio = '<audio id="{num}" controls preload=none type="audio/mpeg" src="/{media_path}/{filename}">' \
+                'not support audio</audio>'
+        html = """<script>
+for (let song = 1; song <= {total}; song++) {{
+  var song_obj = document.getElementById(song);
+  song_obj.onplay = play_start;
+  song_obj.onended = play_next;
+}}
+
+function play_next(evt) {{
+  evt.currentTarget.pause();
+  if (evt.currentTarget.id == {total}) return;
+  next_song = document.getElementById(parseInt(evt.currentTarget.id) + 1);
+  next_song.load();
+  next_song.play();
+}}
+
+var playing = null;
+function play_start(evt) {{
+  if (playing && playing.target.id !== evt.currentTarget.id) playing.target.pause();
+  playing = evt;
+}}
+</script>"""
         client = TelegramClient('parse_tg', config['api_id'], config['api_hash'])
         async with client:
+            text = text_start + '\n\n'
             right_book = False
+            num = 0
             async for message in client.iter_messages(config['channel'], reverse=True):
                 if message.audio:
                     book = message.audio.attributes[0].performer
                     if book == parse_book:
+                        num += 1
                         if not right_book:
                             right_book = True
                         part = message.audio.attributes[0].title
-                        file_name = message.audio.attributes[1].file_name
-                        text = message.text
+                        # file_name = message.audio.attributes[1].file_name
                         path = await message.download_media()
-                        # TODO переместить файл в медиа, создать модель, прописать js
-                        print(book, part, file_name, text[:10])
+                        os.replace(path, f"{media_path}/{path}")
+                        text += f'{message.text}\n{audio.format(num=num, filename=path, media_path=media_path)}\n\n'
+                        print(book, part, path, message.text[:20])
                     elif right_book:
                         break
-                    print(book, message.text[:10])
-                else:
+                    else:
+                        print(book, message.text[:20])
+                elif message.text:
                     print(message.text[:20])
+        news = models.News.objects.create(
+            section=section, author_profile=models.Main.objects.first().profile,
+            title=title, text=text, html=html.format(total=num), author=author
+        )
+        print(news.pk)
